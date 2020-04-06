ad1ff347120ff43c20a1075682708947fcdbcca6
dianbanjiu <dianbanjiu@gmail.com>
2020-04-07 00:09:37 +0800

    Determine whether a tree is symmetric.

--- main.go
+++ main.go
@@ -1,8 +1,4 @@
 package main
 
 func main() {
-	//a := new(TreeNode)
-	//change(a)
-	//fmt.Println(a)
-	//sortedArrayToBST([]int{-10, -3, 0, 5, 9})
 }
--- src/isSymmetric.go
+++ src/isSymmetric.go
@@ -1,65 +1,29 @@
 package src
 
-import "math"
-
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+// 判断一棵树是不是对称
 func IsSymmetric(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	var level = make([]*TreeNode, 0)
-	level = append(level, root)
-	var high = 1
-	var flag = true
-	// 层序遍历
-	for !listIsNil(level) {
-		temp := make([]*TreeNode, int(math.Pow(2, float64(high))))
-		// 遍历获取下一层的数据
-		for i := 0; i < len(level); i++ {
-			if level[i] != nil && level[i].Left != nil {
-				temp[2*i] = level[i].Left
-			}
-			if level[i] != nil && level[i].Right != nil {
-				temp[2*i+1] = level[i].Right
+	var curLevelNode = make([]*TreeNode, 0)
+	curLevelNode = append(curLevelNode, root)
+	for len(curLevelNode) != 0 {
+		for i := 0; i <= len(curLevelNode)/2; i++ {
+			if curLevelNode[i] == nil && curLevelNode[len(curLevelNode)-1-i] != nil ||
+				curLevelNode[i] != nil && curLevelNode[len(curLevelNode)-1-i] == nil ||
+				curLevelNode[i] != nil && curLevelNode[len(curLevelNode)-1-i] != nil &&
+					curLevelNode[i].Val != curLevelNode[len(curLevelNode)-i-1].Val {
+				return false
 			}
 		}
-
-		// 判断下一层的数据是否符合要求
-		if !listIsNil(temp) && !levelJudge(temp) {
-			flag = false
-			break
+		nextLevelNode := make([]*TreeNode, 0)
+		for i := 0; i < len(curLevelNode); i++ {
+			if curLevelNode[i] != nil {
+				nextLevelNode = append(nextLevelNode, curLevelNode[i].Right, curLevelNode[i].Left)
+			}
 		}
-		level = temp
-		high++
-	}
-	return flag
-}
 
-func listIsNil(t []*TreeNode) bool {
-	var flag = true
-	for _, v := range t {
-		if v != nil {
-			flag = false
-			break
-		}
+		curLevelNode = nextLevelNode
 	}
-	return flag
-}
-func levelJudge(t []*TreeNode) bool {
-	var flag = true
-	for i := 0; i < len(t)/2; i++ {
-		if (t[i] == nil && t[len(t)-1-i] != nil) || (t[i] != nil && t[len(t)-1-i] == nil) || ((t[i] != nil && t[len(t)-1-i] != nil) && (t[i].Val != t[len(t)-i-1].Val)) {
-			flag = false
-			break
-		}
-	}
-	return flag
-
+	return true
 }
