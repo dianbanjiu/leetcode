b4946a1ff4050e1428cad1f4abfd3fde23df598a
dianbanjiu <dianbanjiu@gmail.com>
2020-12-13 23:32:19 +0800

    创建每日一题

---
+++ perday/containsDuplicate.go
@@ -0,0 +1,13 @@
+package perday
+
+import "sort"
+
+func ContainsDuplicate(nums []int) bool {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return true
+		}
+	}
+	return false
+}
---
+++ test/containsDuplicate_test.go
@@ -0,0 +1,27 @@
+package leetcodeTest
+
+import (
+	"leetcode/perday"
+	"testing"
+)
+
+func TestContainsDuplicate(t *testing.T) {
+	var caseTest = []struct {
+		in  []int
+		out bool
+	}{
+		{[]int{}, false},
+		{[]int{1}, false},
+		{[]int{1, 1}, true},
+		{[]int{1, 2}, false},
+		{[]int{1, 2, 1}, true},
+		{[]int{1,1,2}, true},
+	}
+
+	for _, value := range caseTest {
+		actual := perday.ContainsDuplicate(value.in)
+		if actual != value.out {
+			t.Errorf("in %v, except %v, actual %v", value.in, actual, value.out)
+		}
+	}
+}
