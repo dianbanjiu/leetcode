989ccd27d093570cfaece2fdf4616a04e27faaa6
dianbanjiu <dianbanjiu@gmail.com>
2021-03-17 22:11:01 +0800

    804. 唯一摩尔斯密码词

---
+++ perday/uniqueMorseRepresentations.go
@@ -0,0 +1,43 @@
+package perday
+
+// leetcode-cn number: 804. 唯一摩尔斯密码词
+// url: https://leetcode-cn.com/problems/unique-morse-code-words/
+func UniqueMorseRepresentations(words []string) int {
+	var morseMap = map[string]string{
+		"a":".-",
+		"b":"-...",
+		"c":"-.-.",
+		"d":"-..",
+		"e":".",
+		"f":"..-.",
+		"g":"--.",
+		"h":"....",
+		"i":"..",
+		"j":".---",
+		"k":"-.-",
+		"l":".-..",
+		"m":"--",
+		"n":"-.",
+		"o":"---",
+		"p":".--.",
+		"q":"--.-",
+		"r":".-.",
+		"s":"...",
+		"t":"-",
+		"u":"..-",
+		"v":"...-",
+		"w":".--",
+		"x":"-..-",
+		"y":"-.--",
+		"z":"--..",
+	}
+	var result = make(map[string]bool)
+	for _, word := range words {
+		var morseString string
+		for _, w := range word {
+			morseString += morseMap[string(w)]
+		}
+		result[morseString] = true
+	}
+	return len(result)
+}
