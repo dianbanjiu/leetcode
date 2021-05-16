46e4a3433c4ac1d4874631b4bc0520bb33504df2
dianbanjiu <dianbanjiu@gmail.com>
2021-05-17 00:20:50 +0800

    雪糕的最大数量

---
+++ perday/maxIceCream.go
@@ -0,0 +1,18 @@
+package perday
+
+import "sort"
+
+//leetcode-cn number: 1833. 雪糕的最大数量
+//leetcode url: https://leetcode-cn.com/problems/maximum-ice-cream-bars/
+func MaxIceCream(costs []int, coins int) int {
+    sort.Ints(costs)
+	var sum, count int
+	for _, cost := range costs {
+		sum += cost
+		if sum > coins {
+			break
+		}
+		count++
+	}
+	return count
+}
