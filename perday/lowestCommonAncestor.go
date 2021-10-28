3fb23704a84b68ea4ca3022bd601027404d07c35
dianbanjiu <dianbanjiu@gmail.com>
2021-10-28 22:31:56 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先

---
+++ perday/lowestCommonAncestor.go
@@ -0,0 +1,31 @@
+package perday
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 剑指 Offer 68 - II. 二叉树的最近公共祖先
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root == p || root == q {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil && right != nil {
+		return right
+	}
+	if left != nil && right == nil {
+		return left
+	}
+	return nil
+}
