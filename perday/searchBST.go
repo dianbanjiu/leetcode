fcfd9916e129a6dcf5d78f50a778cee9109d958a
dianbanjiu <dianbanjiu@gmail.com>
2022-02-07 13:28:26 +0800

    二叉搜索树中的搜索 递归求解

---
+++ perday/searchBST.go
@@ -0,0 +1,24 @@
+package perday
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// leetcode-cn number: 700. 二叉搜索树中的搜索
+// leetcode-cn url: https://leetcode-cn.com/problems/search-in-a-binary-search-tree/
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == val {
+		return root
+	} else if root.Val > val {
+		return searchBST(root.Left, val)
+	} else {
+		return searchBST(root.Right, val)
+	}
+}
